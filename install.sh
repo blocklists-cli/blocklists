e9cecd4895725c765ff0a44a00ef0cf65563e070
Dpbm <dpbm136@gmail.com>
2022-12-19 23:14:35 -0300

    fixed multiple folders problem

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ chmod +x $BLOCKLISTS/bin/*
 chmod +x $BLOCKLISTS/blocklists
 
 # link generated to HOME
-ln -s $BLOCKLISTS/generated/ $HOME
+ln -s "$BLOCKLISTS/generated/" $HOME
 
 # blocklists DIR
 echo "export BLOCKLISTS=\$HOME/blocklists" >> $HOME/.bashrc
